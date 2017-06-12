db4dc8052b52f355cbba28edf28cb168b4e03b44
sergio.munoz.cubillo <sergiomunozcubillo@gmail.com>
2017-06-12 19:10:22 +0200

    p53

--- controllers/tip_controller.js
+++ controllers/tip_controller.js
@@ -5,7 +5,11 @@ var Sequelize = require('sequelize');
 // Autoload la pista asociado a :tipId
 exports.load = function (req, res, next, tipId) {
 
-    models.Tip.findById(tipId)
+    models.Tip.findById(tipId, {
+		include: [ 
+			 {model: models.User, as: 'Author'}
+		]
+})
     .then(function (tip) {
         if (tip) {
             req.tip = tip;
